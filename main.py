788a3206a9f0e517aaa20a58ebbec354f7aa2f1d
andaritri <95196590+andaritri@users.noreply.github.com>
2022-01-01 17:09:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 def main():
     id = os.environ.get("ID", 1)
     response = requests.get(f"https://jsonplaceholder.typicode.com/posts/{id}")
+    print(f"::set-output name=json::{response.json()}")
     print(response)
 
     
